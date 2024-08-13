eba76544f30e144595a9edd317ef3a4eb00e59b6
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-13 18:03:59 +0100

    parse latex from mathquilll

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -5,15 +5,21 @@ import "mathquill/build/mathquill.js";
 onMounted(() => {
   const MQ = MathQuill.getInterface(2);
   const mathFieldSpan = document.getElementById("math-field");
-  const config = { autoCommands: "pi theta sqrt sum" };
+  const config = {
+    autoCommands: "pi theta sqrt sum",
+    handlers: {
+      edit: function () {
+        console.log(mathField.latex());
+      },
+    },
+  };
   const mathField = MQ.MathField(mathFieldSpan, config);
 });
 </script>
 <template>
   <div class="bg-gray-100 w-[416px] mt-5">
-    <div
-      class="bg-white bg-opacity-0 w-full px-3 py-5 text-xl"
-      id="math-field"
-    ></div>
+    <div class="bg-white bg-opacity-0 w-full px-3 py-5 text-xl" id="math-field">
+      log(x)
+    </div>
   </div>
 </template>
