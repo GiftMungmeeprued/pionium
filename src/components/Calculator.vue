8bcadca3f636f8453c43720967d498a435e82b57
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-14 17:12:32 +0100

    remove unused components

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,15 +2,11 @@
 import NumberPad from "./NumberPad.vue";
 import Functions from "./Functions.vue";
 import Screen from "./Screen.vue";
-import BasicFunctions from "./BasicFunctions.vue";
-import Exp from "./Exp.vue";
 </script>
 <template>
   <main class="flex flex-col items-center content-end h-screen">
     <Screen class="grow" />
     <Functions />
-    <!-- <Exp /> -->
-    <!-- <BasicFunctions /> -->
     <NumberPad />
   </main>
 </template>
