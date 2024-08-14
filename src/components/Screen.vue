b2f934da57bdfb793aa639a4cdbdfb091fcc880a
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-14 17:12:56 +0100

    improve screen

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -7,35 +7,47 @@ onMounted(() => {
   const MQ = MathQuill.getInterface(2);
   const ce = new ComputeEngine();
   const mathFieldSpan = document.getElementById("math-field");
+  const answerField = document.getElementById("answer");
   const config = {
     autoCommands: "pi sqrt sum",
     sumStartsWithNEquals: true,
     handlers: {
+      edit: function () {
+        staticMath.latex("");
+      },
       enter: function () {
         console.log(mathField.latex());
-        console.log(ce.parse(mathField.latex()).evaluate().value);
+        const calculated = ce.parse(mathField.latex()).evaluate();
+        // console.log(calculated.json.flat());
+        const isRational =
+          Array.isArray(calculated.json) &&
+          (calculated.json.flat().includes("Rational") ||
+            calculated.json.flat().includes("Sqrt"));
+        staticMath.latex(
+          `= ${isRational ? calculated.latex : calculated.value}`
+        );
+        // mathField.blur();
       },
     },
   };
   const mathField = MQ.MathField(mathFieldSpan, config);
-  const answerField = document.getElementById("answer");
-  MQ.StaticMath(answerField);
+  const staticMath = MQ.StaticMath(answerField);
+  // mathField.typedText("sin(");
 });
 </script>
 <template>
-  <div class="bg-gray-100 w-[416px] mt-5 border-2 border-red-500">
-    <div class="w-full px-3 py-5 text-xl" id="math-field"></div>
-    <div class="bg-red-100 h-10 flex items-center justify-end">
-      <span class="bg-blue-100" id="answer">=sin(45)</span>
+  <div class="bg-gray-100 w-[416px] mt-5 border-2 border-gray-500">
+    <div class="w-full px-3 py-5 text-5xl" id="math-field"></div>
+    <div class="px-3 min-h-10 bg-gray-200 flex items-center justify-end">
+      <span id="answer"></span>
     </div>
   </div>
 </template>
 <style scoped>
 #math-field {
   border-width: 0;
-  background: rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0);
 }
-
 #math-field.mq-editable-field.mq-focused,
 #math-field.mq-math-mode .mq-editable-field.mq-focused {
   -webkit-box-shadow: none;
