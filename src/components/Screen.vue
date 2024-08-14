37f62d8b1c03e582311ce5d155fb0bbcc16161d1
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-14 14:16:33 +0100

    configure screen layout

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -1,32 +1,48 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import "mathquill/build/mathquill.min.js";
+import { ComputeEngine } from "https://unpkg.com/@cortex-js/compute-engine?module";
 
-const latex = ref("sd");
 onMounted(() => {
   const MQ = MathQuill.getInterface(2);
+  const ce = new ComputeEngine();
   const mathFieldSpan = document.getElementById("math-field");
   const config = {
-    autoCommands: "pi theta sqrt sum",
+    autoCommands: "pi sqrt sum",
+    sumStartsWithNEquals: true,
     handlers: {
-      edit: function () {
-        latex.value = mathField.latex();
-      },
       enter: function () {
-        console.log(latex.value);
-        let a = evaluatex(latex.value)();
-        console.log(a);
+        console.log(mathField.latex());
+        console.log(ce.parse(mathField.latex()).evaluate().value);
       },
     },
   };
   const mathField = MQ.MathField(mathFieldSpan, config);
+  const answerField = document.getElementById("answer");
+  MQ.StaticMath(answerField);
 });
 </script>
 <template>
-  <div class="bg-gray-100 w-[416px] mt-5">
-    <div
-      class="bg-white bg-opacity-0 w-full px-3 py-5 text-xl"
-      id="math-field"
-    ></div>
+  <div class="bg-gray-100 w-[416px] mt-5 border-2 border-red-500">
+    <div class="w-full px-3 py-5 text-xl" id="math-field"></div>
+    <div class="bg-red-100 h-10 flex items-center justify-end">
+      <span class="bg-blue-100" id="answer">=sin(45)</span>
+    </div>
   </div>
 </template>
+<style scoped>
+#math-field {
+  border-width: 0;
+  background: rgba(0, 0, 0, 0.2);
+}
+
+#math-field.mq-editable-field.mq-focused,
+#math-field.mq-math-mode .mq-editable-field.mq-focused {
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  border-width: 0;
+  /* border-color: white; */
+  border-radius: 1px;
+}
+</style>
