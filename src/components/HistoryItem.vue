6b0a035afb61bf4b0e490c7034a5b402a3fcf4ca
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-27 16:08:06 +0100

    edit variable names

--- src/components/HistoryItem.vue
+++ src/components/HistoryItem.vue
@@ -7,20 +7,20 @@ const props = defineProps({
     default: {},
   },
 });
-const inputEl2 = ref(null);
-const answerEl2 = ref(null);
+const inputEl = ref(null);
+const answerEl = ref(null);
 
 onMounted(() => {
   const MQ = MathQuill.getInterface(2);
-  MQ.StaticMath(inputEl2.value).latex(props.item.input);
-  MQ.StaticMath(answerEl2.value).latex(props.item.answer);
+  MQ.StaticMath(inputEl.value).latex(props.item.input);
+  MQ.StaticMath(answerEl.value).latex(props.item.answer);
 });
 </script>
 <template>
   <div
     class="pt-5 pb-3 px-5 border-solid border-b border-0 border-neutral-300 hover:bg-neutral-100"
   >
-    <div class="h-8 w-full" ref="inputEl2"></div>
-    <div class="h-8 w-full text-right" ref="answerEl2"></div>
+    <div class="h-8 w-full" ref="inputEl"></div>
+    <div class="h-8 w-full text-right" ref="answerEl"></div>
   </div>
 </template>
