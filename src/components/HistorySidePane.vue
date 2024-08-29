7af9b03effbb21c0c4d109fb27b4f145378ba3de
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-08-29 22:54:46 +0100

    add no history svg

--- src/components/HistorySidePane.vue
+++ src/components/HistorySidePane.vue
@@ -1,27 +1,19 @@
 <script setup>
 import HistoryList from "./HistoryList.vue";
 import SidePane from "./SidePane.vue";
+import { store } from "./store";
 </script>
 
 <template>
   <SidePane sidepane-id="historySidePane" sidepane-label="history sidepane">
     <HistoryList />
+    <div id="no-history" class="text-center" v-if="store.history.length === 0">
+      <img
+        src="../assets/no-history.svg"
+        alt="Empty history icon"
+        class="m-auto my-10 w-[200px]"
+      />
+      No calculation history yet
+    </div>
   </SidePane>
 </template>
-<!-- <style>
-#history-sidepane {
-  /* width: 206px; */
-  width: 416px;
-}
-@media (max-width: 416px) {
-  .offcanvas {
-    transition: none !important;
-  }
-}
-
-@media (min-width: 640px) {
-  .offcanvas {
-    visibility: hidden;
-  }
-}
-</style> -->
