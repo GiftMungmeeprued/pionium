f3cd5ca39eca3b705ab7b7f7e8a79e27de618f3c
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-09-07 17:22:47 +0100

    remove focus on click

--- src/components/NumberPad.vue
+++ src/components/NumberPad.vue
@@ -4,55 +4,88 @@ import { ArrowTurnDownLeftIcon } from "@heroicons/vue/16/solid";
 </script>
 <template>
   <div class="bg-[#E9EAEE] grid grid-cols-5 gap-1 p-1.5 py-3">
-    <Button main-title="Seven" @click="$emit('typedText', '7')"> 7 </Button>
-    <Button main-title="Eight" @click="$emit('typedText', '8')"> 8</Button>
-    <Button main-title="Nine" @click="$emit('typedText', '9')"> 9</Button>
-    <Button main-title="Divide" @click="$emit('typedText', '/')">÷</Button>
+    <Button main-title="Seven" @mousedown.prevent="$emit('typedText', '7')">
+      7
+    </Button>
+    <Button main-title="Eight" @mousedown.prevent="$emit('typedText', '8')">
+      8</Button
+    >
+    <Button main-title="Nine" @mousedown.prevent="$emit('typedText', '9')">
+      9</Button
+    >
+    <Button main-title="Divide" @mousedown.prevent="$emit('typedText', '/')"
+      >÷</Button
+    >
     <Button
       main-title="Clear screen"
-      @click="$emit('keystroke', 'AC')"
+      @mousedown.prevent="$emit('keystroke', 'AC')"
       color="gray"
     >
       AC
     </Button>
-    <Button main-title="Four" @click="$emit('typedText', '4')">4</Button>
-    <Button main-title="Five" @click="$emit('typedText', '5')">5</Button>
-    <Button main-title="Six" @click="$emit('typedText', '6')">6</Button>
-    <Button main-title="Multiply" @click="$emit('typedText', '*')">×</Button>
+    <Button main-title="Four" @mousedown.prevent="$emit('typedText', '4')"
+      >4</Button
+    >
+    <Button main-title="Five" @mousedown.prevent="$emit('typedText', '5')"
+      >5</Button
+    >
+    <Button main-title="Six" @mousedown.prevent="$emit('typedText', '6')"
+      >6</Button
+    >
+    <Button main-title="Multiply" @mousedown.prevent="$emit('typedText', '*')"
+      >×</Button
+    >
     <Button
       main-title="Answer from previous calculation"
-      @click="$emit('typedText', 'Ans')"
+      @mousedown.prevent="$emit('typedText', 'Ans')"
       color="gray"
       >Ans</Button
     >
-    <Button main-title="One" @click="$emit('typedText', '1')">1</Button>
-    <Button main-title="Two" @click="$emit('typedText', '2')">2</Button>
-    <Button main-title="Three" @click="$emit('typedText', '3')">3</Button>
-    <Button main-title="Subtract" @click="$emit('typedText', '-')">–</Button>
+    <Button main-title="One" @mousedown.prevent="$emit('typedText', '1')"
+      >1</Button
+    >
+    <Button main-title="Two" @mousedown.prevent="$emit('typedText', '2')"
+      >2</Button
+    >
+    <Button main-title="Three" @mousedown.prevent="$emit('typedText', '3')"
+      >3</Button
+    >
+    <Button main-title="Subtract" @mousedown.prevent="$emit('typedText', '-')"
+      >–</Button
+    >
 
     <Button
-      @click="$emit('keystroke', 'Backspace')"
+      @mousedown.prevent="$emit('keystroke', 'Backspace')"
       main-title="Delete backwards"
       color="gray"
     >
       <i class="pi pi-delete-left"></i
     ></Button>
-    <Button main-title="Zero" @click="$emit('typedText', '0')">0</Button>
-    <Button main-title="Decimal point" @click="$emit('typedText', '.')"
+    <Button main-title="Zero" @mousedown.prevent="$emit('typedText', '0')"
+      >0</Button
+    >
+    <Button
+      main-title="Decimal point"
+      @mousedown.prevent="$emit('typedText', '.')"
       >.</Button
     >
-    <Button main-title="Power of 10" @click="$emit('typedText', '*10^')">
+    <Button
+      main-title="Power of 10"
+      @mousedown.prevent="$emit('typedText', '*10^')"
+    >
       <span class="text-sm relative tall:text-3xl">
         ×10<sup
           ><math><mi>x</mi></math></sup
         >
       </span>
     </Button>
-    <Button main-title="Add" @click="$emit('typedText', '+')">+</Button>
+    <Button main-title="Add" @mousedown.prevent="$emit('typedText', '+')"
+      >+</Button
+    >
 
     <Button
       main-title="Calculate"
-      @click="$emit('keystroke', 'Enter')"
+      @mousedown.prevent="$emit('keystroke', 'Enter')"
       color="blue"
     >
       <ArrowTurnDownLeftIcon class="size-6 tall:size-8 inline" />
