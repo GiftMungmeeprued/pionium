57573e91086da030f4bf20e0855b6374afd22e40
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-09-13 17:41:32 +0100

    added donate button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,6 +16,20 @@ import Logo from "./Logo.vue";
         <div
           class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
+          <a href="https://ko-fi.com/pionium" target="_blank">
+            <div
+              class="rounded-full bg-[#434b57] px-2 mr-2 tall:py-1 tall:px-3 tall:mr-5"
+            >
+              <img
+                src="../assets/kofi_logo.png"
+                alt="Donate on Ko-fi"
+                class="w-[20px] mb-[3px] mr-[5px] inline tall:w-[30px] tall:mb-[5px]"
+              /><span
+                class="text-white align-[1px] mr-[3px] text-xs nunito-regular tall:text-base"
+                >Donate</span
+              >
+            </div>
+          </a>
           <SidePaneButton
             sidepane-id="historySidePane"
             sidepane-label="history sidepane"
@@ -35,3 +49,12 @@ import Logo from "./Logo.vue";
     </div>
   </nav>
 </template>
+
+<style scoped>
+.nunito-regular {
+  font-family: "Nunito", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+</style>
