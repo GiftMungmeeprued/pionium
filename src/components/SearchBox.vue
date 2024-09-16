c06c0d53a9c0e5b2f772714cc491ddafeee0091d
GiftMungmeeprued <t.mungmeeprued@gmail.com>
2024-09-16 22:42:07 +0100

    adjusted searchbox size

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -87,7 +87,7 @@
           <!-- SearchBox Modal Body -->
           <div class="!mt-0" data-hs-combo-box-output="">
             <div
-              class="h-[27svh] tall:max-h-[500px] p-2 overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+              class="h-[27svh] tall:h-[500px] p-2 overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
               data-hs-combo-box-output-items-wrapper=""
             ></div>
           </div>
